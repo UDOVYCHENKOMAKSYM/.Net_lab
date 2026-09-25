3404842d242dbbd1cc7e046a9b5cdad102a49fb4
Maksym Udovychenko <udov07max@gmail.com>
2026-09-25 18:29:54 +0300

    .

---
+++ Lab1/Lab1/Class_lab2/Calculation_abc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.Class_lab2
+{
+    public class Calculation_abc
+    {
+        int a, b, c;
+        public Calculation_abc(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public Calculation_abc()
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+        }
+        public Calculation_abc(Calculation_abc obj)
+        {
+            this.a = obj.a;
+            this.b = obj.b;
+            this.c = obj.c;
+        }
+        public int A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+                public int B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public int C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        public int D ()
+        {
+            int sum = 0;
+            if(a%7==0)
+            {
+                sum += a*a*a;
+            }
+            if(b % 7 == 0)
+            {
+                sum += b*b*b;
+            }
+            if(c % 7 == 0)
+            {
+                sum += c*c*c;
+            }
+            return sum;
+        }
+    }
+}
---
+++ TestProject/Calculation_abcTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Lab1.Class_lab2;
+namespace TestProject
+{
+    [TestClass]
+    public class Calculation_abcTest
+    {
+        [TestMethod]
+        public void D_AllNumbersDivisibleBy7_ReturnsSumOfCubes()
+        {
+            var cal=new Calculation_abc(7, 14, 21);
+            var testRes = 12348;
+            var res= cal.D();
+            Assert.AreEqual(testRes, res);
+        }
+        [TestMethod]
+        public void D_NoNumbersDivisibleBy7_ReturnsZero()
+        {
+            var cal = new Calculation_abc(1, 2, 3);
+            var testRes = 0;
+            var res = cal.D();
+            Assert.AreEqual(testRes, res);
+        }
+        [TestMethod]
+        public void D_TwoNumbersDivisibleBy7_ReturnsSumOfCubes()
+        {
+            var cal = new Calculation_abc(7, 2, 21);
+            var testRes = 9604;
+            var res = cal.D();
+            Assert.AreEqual(testRes, res);
+        }
+        [TestMethod]
+        public void D_OneNumberDivisibleBy7_ReturnsSumOfCubes()
+        {
+            var cal = new Calculation_abc(7, 2, 3);
+            var testRes = 343;
+            var res = cal.D();
+            Assert.AreEqual(testRes, res);
+        }
+
+    }
+}
